5e4bb761428100f5774dd73fce4804f601827c17
atiht dangpradub <Student@Train11-1-PC320.control.ad.sit.kmutt.ac.th>
2026-03-11 16:24:46 +0700

    setup web

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+import { ref } from 'vue'
+
+// Menu: About, Careers, Events, Products, Supports
+const menuItems = ref([
+  {name:"About", link:"#"},
+  {name:"Careers", link:"#"},
+  {name:"Events", link:"#"},
+  {name:"Products", link:"#"},
+  {name:"Supports", link:"#"}
+])
+
+</script>
+
+<template>
+  
+  <div class="h-screen">
+
+    <Navbar :menuItems="menuItems"/>
+
+  </div>
+  
+</template>
+
+<style scoped></style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<script setup>
+defineProps({
+    menuItems: {
+        type: Array,
+        required: true
+    }
+})
+</script>
+
+<template>
+  
+    <section class="bg-[url(assets/mobile/image-hero.jpg)] bg-no-repeat bg-cover 
+    md:bg-[url(assets/desktop/image-hero.jpg)]">
+        <div class="container max-w-6xl mx-auto px-6 py-12">
+            <nav class="flex items-center justify-between font-bold text-white">
+                <img src="../assets/logo.svg" alt="">
+                <!-- desktop -->
+                <div class="hidden h-10 font-sans md:flex md:space-x-8">
+                    <div v-for="item in menuItems" :key="item.name" class="group">
+                        <a :href="item.link">{{ item.name }}</a>
+                        <div class="mx-2 group-hover:border-b group-hover:border-blue-50"></div>
+                    </div>
+                </div>
+                <!-- mobile -->
+                <img src="../assets/icon-hamburger.svg" alt="" class="md:hidden">
+            </nav>
+        </div>
+    </section>
+  
+</template>
+
+<style scoped></style>
