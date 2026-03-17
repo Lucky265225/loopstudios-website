68e9d9289c1c76497c9a453017bf56304c013c42
Jackapong Karapinvong <129889438+Lucky265225@users.noreply.github.com>
2026-03-18 01:00:31 +0700

    hamburger event click

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,4 +1,8 @@
 <script setup>
+import { ref } from 'vue'
+
+const hamburger = ref(false)
+
 defineProps({
     menuItems: {
         type: Array,
@@ -13,7 +17,7 @@ defineProps({
     md:bg-[url(assets/desktop/image-hero.jpg)]">
         <div class="container max-w-6xl mx-auto px-6 py-12">
             <nav class="flex items-center justify-between font-bold text-white">
-                <img src="../assets/logo.svg" alt="">
+                <img src="../assets/logo.svg" alt="" @click="">
                 <!-- desktop -->
                 <div class="hidden h-10 font-sans md:flex md:space-x-8">
                     <div v-for="item in menuItems" :key="item.name" class="group">
@@ -22,7 +26,13 @@ defineProps({
                     </div>
                 </div>
                 <!-- mobile -->
-                <img src="../assets/icon-hamburger.svg" alt="" class="md:hidden">
+                <img src="../assets/icon-hamburger.svg" alt="" class="md:hidden" @click="hamburger = !hamburger">
+                <!-- hamburger -->
+                <div class="fixed top-0 right-0 w-full h-screen md:hidden bg-black text-white flex flex-col items-center p-5 space-y-4
+                transform transition-transform duration-300" :class="hamburger ? 'translate-x-0' : 'translate-x-full'">
+                    <img src="../assets/icon-close.svg" alt="" class="ml-auto" @click="hamburger = !hamburger">
+                    <a v-for="item in menuItems" :href="item.link">{{ item.name }}</a>
+                </div>
             </nav>
         </div>
     </section>
